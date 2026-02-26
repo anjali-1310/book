0c1bb3ec1f5e70de11a324d19f99b6bc770abe24
anjali-1310 <109529781+anjali-1310@users.noreply.github.com>
2026-02-26 22:11:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("📚 Book Recommendation System")
 
 books = pd.read_csv("books_small.csv")
 
-selected_book = st.selectbox("Choose a book", books["title"].values)
+selected_book = st.selectbox("Choose a book", books["Book-Title"].values)
 
 if st.button("Recommend"):
     recs = books.sample(5)  # replace with your model later
